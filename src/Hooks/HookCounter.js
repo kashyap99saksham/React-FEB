5e2f64b70ee9aff9a3ca7a6c1f44f833534e0abf
kashyap99saksham <2017kucp1056@iiitkota.ac.in>
2022-03-02 21:32:22 +0530

    useEffect

--- src/Hooks/HookCounter.js
+++ src/Hooks/HookCounter.js
@@ -1,23 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const HookCounter = () => {
 
-    const [ name, setName ] = useState( { firstName : '' , lastName: '' } )
+    const [ items, setItems ] = useState( "" )
+
+    useEffect( () => {
+        console.log("hiiii");
+    } )
 
     return (
-        <form>
-            <h1>{ JSON.stringify(name) } </h1>
-            
-            <input type='text' value = {name.firstName} onChange={ 
-                (e)=> setName( { ...name, firstName: e.target.value } ) 
-            } />
-
-            
-            <input type='text' value={name.lastName} onChange={ 
-                (e)=> setName( { ...name, lastName: e.target.value } ) 
-            } />
-            
-        </form>
+       <div>
+           {items}
+           <button onClick={ ()=>setItems("saksham") }> Change Value </button>
+       </div>
     )
 }
 
@@ -35,3 +30,48 @@ export default HookCounter
 // sum(...arrVal)
 
 
+
+
+// items = [ 
+//     {
+//         id:1,
+//         value:10
+//     },
+//     {
+//         id:2,
+//         value: 4
+//     },
+//     {
+//         id:3,
+//         value: 2
+//     }
+// ]
+
+// items.map( (val)=>{
+//     <li> {val.id}{val.value} </li>
+// } )
+
+{/* <li> saksham </li>
+<li> kashyap </li>
+<li> name </li> */}
+
+
+
+
+// useState hook allow to use state in functional component.
+// In classes , the state is always an Object.
+// useState hook return an array with 2 elelent.
+// first element is current value,
+// second element is a state setter function.
+// when working with object or array, always make sure to use spread operator.
+// use key when using map
+
+
+// componentDidMount() {
+//     // logic 1
+//     // logic 2
+//     // logic 3
+// }
+// componentDidUpdate() {
+
+// }
